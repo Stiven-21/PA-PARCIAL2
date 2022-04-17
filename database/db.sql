5f79dd5ce6e4b24dd4bd696f0c8dc2f37df3d69b
Stiven-21 <stiven.cor2002@gmail.com>
2022-04-16 22:30:40 -0500

    ENLAZANDO A HEROKU

--- database/db.sql
+++ database/db.sql
@@ -21,17 +21,23 @@ USE `parcial2`;
 CREATE TABLE IF NOT EXISTS `archivos` (
   `id_archivo` bigint(20) NOT NULL AUTO_INCREMENT,
   `id_usuario` bigint(20) DEFAULT NULL,
-  `nombre_archivo` varchar(50) DEFAULT NULL,
+  `nombre_archivo` varchar(255) DEFAULT NULL,
   `ruta_archivo` varchar(500) DEFAULT NULL,
   `ruta_vista` varchar(500) DEFAULT NULL,
   `type` varchar(500) DEFAULT NULL,
+  `size` varchar(20) DEFAULT NULL,
   `accesso` varchar(3) DEFAULT NULL,
+  `url_share` varchar(100) DEFAULT NULL,
   PRIMARY KEY (`id_archivo`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=utf8;
 
 -- Volcando datos para la tabla parcial2.archivos: ~0 rows (aproximadamente)
 DELETE FROM `archivos`;
 /*!40000 ALTER TABLE `archivos` DISABLE KEYS */;
+INSERT INTO `archivos` (`id_archivo`, `id_usuario`, `nombre_archivo`, `ruta_archivo`, `ruta_vista`, `type`, `size`, `accesso`, `url_share`) VALUES
+	(1, 1, 'prueba prueba prueba  1', 'prueba-prueba-prueba--1-2022-04-16-21-50-6-186419.mp3', './static/images/types/no-image.jpg', 'mp3', '3.7 MB', 'on', '7y3vvQQ0aCeUaurK27t2arhDDHQU20xGx1hchFcB9DG9XNJSDuDgMRAhWMYkdbXbmajbflU1FdF7JtAOOSfji5hjbP'),
+	(2, 1, 'prueba prueba prueba  1 ', 'prueba-prueba-prueba--1--2022-04-16-21-50-20-570441.exe', './static/images/types/ejecutable.png', 'exe', '4.9 MB', 'on', '7GJFt9g8BgnXUYYwLHARi5lCp1k1ZB19Xg3j5uvdebTepW6ev5DjQ4MCF5RRuFunEM7qeuq8Rd16jYPdJsfNPKf7oR'),
+	(3, 1, 'prueba prueba prueba  1 ', 'prueba-prueba-prueba--1--2022-04-16-21-50-36-314450.png', './static/images_server/prueba-prueba-prueba--1--2022-04-16-21-50-36-314450.png', 'png', '1.4 MB', 'off', 'x0ugCW9v1p4C7phsmXZInWjRVhPg2kYClep544Whl3JsL73WoHfXGj0bHcjPxmyam5phPYGxVUphQcuRQ1qG8eokCj');
 /*!40000 ALTER TABLE `archivos` ENABLE KEYS */;
 
 -- Volcando estructura para tabla parcial2.usuarios
@@ -52,7 +58,7 @@ DELETE FROM `usuarios`;
 /*!40000 ALTER TABLE `usuarios` DISABLE KEYS */;
 INSERT INTO `usuarios` (`id_usuario`, `nombre_usuario`, `apellido_usuario`, `user`, `password`, `validate`, `url_val_mail`, `url_pass`) VALUES
 	(1, 'James', 'Cordoba', 'zujuroquemma-2730@yopmail.com', 'df548fa419918be525b32cd27570a925e1311b480cc329067d3940639cf3eaa584727efd2fd2fa5c3d9443835dc0bcb0f74eb06a669565d1d47ea0ad202888c5', 'true', '', ''),
-	(2, 'James', '', 'sofreissaxeja-5176@yopmail.com', '83100ca686653af97f12ec4be588b4de535318ebe439051be1e98cc70b8604f525f4ee2dbeee60b22d9bf23cecfeeecf9f789ec99e3fe8482449e686fb9924ab', 'true', '', '3cbqxzj1EkhDFPOWvFHXivNkvOGAZM7I25eLYoEMluZu6pGL1aazzc2vqkeH');
+	(2, 'James', '', 'sofreissaxeja-5176@yopmail.com', 'df548fa419918be525b32cd27570a925e1311b480cc329067d3940639cf3eaa584727efd2fd2fa5c3d9443835dc0bcb0f74eb06a669565d1d47ea0ad202888c5', 'true', '', '');
 /*!40000 ALTER TABLE `usuarios` ENABLE KEYS */;
 
 /*!40101 SET SQL_MODE=IFNULL(@OLD_SQL_MODE, '') */;
